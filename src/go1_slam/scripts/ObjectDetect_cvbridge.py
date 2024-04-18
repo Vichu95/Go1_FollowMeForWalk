a36e4fd8599eae95ff2962ee7c81b707546a9b72
Vishnu <k.vishnudev@gmail.com>
2024-04-18 16:14:12 +0200

    Tried to access directly the numpy array of image ros

--- src/go1_slam/scripts/ObjectDetect_cvbridge.py
+++ src/go1_slam/scripts/ObjectDetect_cvbridge.py
@@ -6,30 +6,33 @@ import rospy
 import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
 
   def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic_2",Image)
 
+    self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/front/image",Image,self.callback)
 
   def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
 
-    width = data.width
-    height = data.height
-    img_data = np.frombuffer(data.data, dtype=np.uint8)
-    image = img_data.reshape((height, width, 3))
-    # image_cv = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR if needed
+    (rows,cols,channels) = cv_image.shape
+    if cols > 60 and rows > 60 :
+      cv2.circle(cv_image, (50,50), 10, 255)
 
-
-    # (rows,cols,channels) = cv_image.shape
-    # if cols > 60 and rows > 60 :
-    cv2.circle(image, (50,50), 10, 255)
-
-    # cv2.imshow("Image window", cv_image)
-    cv2.imshow("ROS Image", image)
+    cv2.imshow("Image window", cv_image)
     cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
 
 def main(args):
   ic = image_converter()
